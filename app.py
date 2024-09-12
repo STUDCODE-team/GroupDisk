fb1c3e3260e5b4282d7db411d93d5bad85a82fa3
Ignatiy Glushkov <ignat.glushkov@mail.ru>
2024-09-13 00:18:22 +0300

    update login page without logic

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if email == 'test@test.com' and password == 'test':
+            return redirect(url_for('success'))
+        else:
+            return 'Неверный email или пароль'
+
+    return render_template('auth.html')
+
+# Страница успешного входа
+@app.route('/success')
+def success():
+    return 'Вы успешно вошли!'
 
 if __name__ == '__main__':
   app.run(debug=True)
